2f6755addf6b7a2fb9a362ff2de421d7d694c146
Alexandre Oliveira <alexandre@55seeds.com>
2025-11-26 21:50:33 +0400

    Update fastskill.rb

--- Formula/fastskill.rb
+++ Formula/fastskill.rb
@@ -10,7 +10,7 @@ class Fastskill < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/gofastskill/fastskill/releases/download/v0.7.10/fastskill-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "sha256:66b5d14600c607618431e851dcf5f19be7d602eab94171237033073e442ce945"
+      sha256 "66b5d14600c607618431e851dcf5f19be7d602eab94171237033073e442ce945"
     end
   end
 
