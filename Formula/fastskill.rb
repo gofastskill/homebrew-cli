443b7c48e780ba4ab18782ff3fbcfe3595c974ad
Alexandre Oliveira <alexandre@55seeds.com>
2025-12-01 06:56:47 +0000

    Update fastskill to version 0.7.14

--- Formula/fastskill.rb
+++ Formula/fastskill.rb
@@ -4,7 +4,7 @@
 class Fastskill < Formula
   desc "Fastskill - A fast skill management tool"
   homepage "https://github.com/gofastskill/fastskill"
-  version "0.7.13"
+  version "0.7.14"
   license "Apache-2.0"
 
   on_linux do
@@ -19,10 +19,10 @@ class Fastskill < Formula
       end
 
       if glibc_version >= 2.38
-        url "https://github.com/gofastskill/fastskill/releases/download/v0.7.13/fastskill-x86_64-unknown-linux-gnu.tar.gz"
+        url "https://github.com/gofastskill/fastskill/releases/download/v0.7.14/fastskill-x86_64-unknown-linux-gnu.tar.gz"
         sha256 "ab4a1791e08f4a144eaadef83aab4e312a5fee0bd2a169880ca85966f6f730b4"
       else
-        url "https://github.com/gofastskill/fastskill/releases/download/v0.7.13/fastskill-x86_64-unknown-linux-musl.tar.gz"
+        url "https://github.com/gofastskill/fastskill/releases/download/v0.7.14/fastskill-x86_64-unknown-linux-musl.tar.gz"
         sha256 "1f0ccae7a9a6f8f027be0c251692eaf5fc2b1c9a543d527b683841e4bd2a6ca6"
       end
     end
